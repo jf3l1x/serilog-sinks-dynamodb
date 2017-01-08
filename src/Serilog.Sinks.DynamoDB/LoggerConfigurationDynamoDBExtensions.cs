4da5d66cda71eb82d99e998366f789a93ee23e54
Jose Felix <jose.felix@sapiensia.com>
2017-01-08 21:42:02 -0200

    Updated nugets to suport serilog 2.3, added better formatting for properties , using the formater from Sinks.Serilog

--- src/Serilog.Sinks.DynamoDB/LoggerConfigurationDynamoDBExtensions.cs
+++ src/Serilog.Sinks.DynamoDB/LoggerConfigurationDynamoDBExtensions.cs
@@ -32,20 +32,20 @@ namespace Serilog
     /// </summary>
     /// <param name="loggerConfiguration">The logger configuration.</param>
     /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
-    /// <param name="tableName">The DynamoDB Tabel to log to</param>
+    /// <param name="options">The DynamoDB options</param>
     /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
     /// <returns>Logger configuration, allowing configuration to continue.</returns>
     /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
     public static LoggerConfiguration DynamoDB(
       this LoggerSinkConfiguration loggerConfiguration,
-      string tableName,
+      DynamoDBSinkOptions options,
       IFormatProvider formatProvider = null,
       LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum
       )
     {
       if (loggerConfiguration == null) throw new ArgumentNullException("loggerConfiguration");
 
-      var logEventSink = new DynamoDBSink(formatProvider, tableName);
+      var logEventSink = new DynamoDBSink(formatProvider, options);
 
       return loggerConfiguration.Sink(logEventSink, restrictedToMinimumLevel);
     }
